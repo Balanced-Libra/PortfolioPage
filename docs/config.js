c116965b49158a9fc0d49175e3e73ca025bd8f67
Julius <guyjamesjulius@gmail.com>
2025-04-26 17:44:23 +0200

    Add files via upload

---
+++ docs/config.js
@@ -0,0 +1,6 @@
+// config.js
+const emailConfig = {
+  publicKey: '3aimvoUWnig58tBKe', // Replace with new key from EmailJS
+  serviceId: 'service_0w4mbpe', // Replace with new service ID
+  templateId: 'template_mtttn0q' // Replace with new template ID
+};
